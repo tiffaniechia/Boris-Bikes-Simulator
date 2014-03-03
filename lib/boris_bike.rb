b44eb49ffdb228ec5bd9b8fea52f914ca75189ab
tiffanie <yt.chia.2009@smu.edu.sg>
2014-03-03 19:12:48 +0000

    minor refactoring

--- lib/boris_bike.rb
+++ lib/boris_bike.rb
@@ -1,7 +1,7 @@
 class Bike
 
 	def initialize
-		@broken = false
+	fix
 	end	
 
 	def broken?
@@ -12,8 +12,8 @@ class Bike
 	@broken = true
 	end	
 
-	def fixed
+	def fix
 	@broken = false
 	end	
 
-end
+ends
