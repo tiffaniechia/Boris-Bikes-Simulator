616409d03cf3d413586d8044f0ff80de228e42bf
tiffanie <yt.chia.2009@smu.edu.sg>
2014-03-03 20:38:46 +0000

    docking station

--- lib/boris_bike.rb
+++ lib/boris_bike.rb
@@ -19,17 +19,32 @@ class Bike
 end
 
 class Dockingstation
+	DEFAULT_CAPACITY = 10
 
-	def initialize
+	def initialize(options = {})
+		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
 		@bikes = [] 
-	end
-
+	end 
 	def bike_count
 		@bikes.count
 	end
 
 	def dock(bike)
-		@bikes << bike
+		raise "Station is full" if full?
+ 		@bikes << bike
 	end
+
+	def release(bike)
+		@bikes.delete(bike)
+	end
+
+	def full?
+		bike_count == @capacity
+	end
+
+	def available_bikes
+		@bikes.reject {|bike| bike.broken?}
+	end	
+
 
 end
--- spec/boris_bike_spec.rb
+++ spec/boris_bike_spec.rb
@@ -29,12 +29,42 @@ end
 
 describe Dockingstation do 
 
+	let(:bike) {Bike.new}
+	let(:station) {Dockingstation.new}
+
+	def fill_station(station)
+		10.times {station.dock(Bike.new)}
+	end
+
 	it "should accept a bike" do
-		bike = Bike.new
-		station = Dockingstation.new
 		expect(station.bike_count).to eq (0)
 		station.dock(bike)
 		expect(station.bike_count).to eq(1)
 	end
 		
+	it "should release a bike" do 
+		 station.dock(bike)
+		 station.release(bike)
+		 expect(station.bike_count).to eq(0)
+	end	 
+
+	it "should know when its full" do 
+		expect(station).not_to be_full
+		fill_station station
+		expect(station).to be_full
+	end	
+
+	it "should not accept a bike if its full" do 
+		fill_station station
+		expect(lambda {station.dock(bike)}).to raise_error(RuntimeError)
+	end	
+
+	it "should provide the list of available bikes" do 
+		working_bike, broken_bike = Bike.new, Bike.new
+		broken_bike.break
+		station.dock(working_bike)
+		station.dock(broken_bike)
+		expect(station.available_bikes).to eq([working_bike])
+	end	
+
 end	
