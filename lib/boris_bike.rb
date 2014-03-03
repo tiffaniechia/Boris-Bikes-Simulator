c164d38f21d7ff163c7b7d142b66ccbb38c1a5a4
tiffanie <yt.chia.2009@smu.edu.sg>
2014-03-03 18:33:44 +0000

    first test written

---
+++ lib/boris_bike.rb
@@ -0,0 +1,6 @@
+class Bike
+
+	def broken?
+	end	
+
+end
---
+++ spec/boris_bike_spec.rb
@@ -0,0 +1,10 @@
+require '../lib/boris_bike'
+
+describe Bike do 
+	it "should not be broken after we create it" do 
+		bike = Bike.new
+		expect(bike).not_to be_broken
+		#expect(bike.broken?).to be_false
+	end
+end
+
