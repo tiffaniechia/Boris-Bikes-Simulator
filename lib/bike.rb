ed4129511a15da0b79221af63f428046fb29cfaf
tiffaniechia <tiffaniekyrachia@gmail.com>
2014-05-16 20:41:26 +0800

    reattempting boris bikes from scratch. first commit

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,25 +1,6 @@
 class Bike
 
-	def initialize
-	fix
-	end	
+  def broken?
+  end  
 
-	def broken?
-	@broken
-	end
-	
-	def break
-	@broken = true
-	end	
-
-	def fix
-	@broken = false
-	end	
-
-end
-
-
-
-
-
-
+end 
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,28 +1,11 @@
-require '../lib/bike'
+require_relative "../lib/bike"
 
-describe Bike do 
+describe Bike do
 
-	let(:bike) {Bike.new}
+  it "should not be broken after we create it" do
+    bike = Bike.new
+    # expect(bike).not_to be_broken
+    expect(bike.broken?).to be_false
+  end  
 
-	it "should not be broken after we create it" do 
-		bike = Bike.new
-		expect(bike).not_to be_broken
-		#expect(bike.broken?).to be_false
-	end
-
-	it "should be able to break" do 
-		bike = Bike.new 
-		bike.break
-		expect(bike).to be_broken
-	end
-	
-
-	it "should be able to get fixed" do 
-		bike = Bike.new
-		bike.break
-		bike.fix
-		expect(bike).not_to be_broken
-	end
 end
-
-
